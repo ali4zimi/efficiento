c37ae2b8f9970243ee1de21e749eac33fc99a619
Ali Azimi <azimi.ali@tum.de>
2023-05-01 16:34:09 +0200

    fixed storing of data and improved UI design

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,18 @@
 // remove settings from chrome.storage;
 // chrome.storage.sync.remove('settings');
 
+const resetButton = document.querySelector('.reset-button');
+resetButton.addEventListener('click', () => {
+  chrome.storage.sync.set({ settings: defaultSettings });
+
+  // delete todos
+  chrome.storage.sync.remove('todos');
+
+  // reload the page
+  window.location.reload();
+});
+
+
 const defaultSettings = {
   clock: true,
   timeFormat: 12,
@@ -27,6 +39,7 @@ chrome.storage.sync.get().then(data => {
     chrome.storage.sync.set({ todos });
   }
   initializeTodoList();
+  updateClock();
 });
 
 
@@ -51,7 +64,7 @@ const updateClock = () => {
 
 setInterval(() => {
   updateClock();
-}, 100);
+}, 500);
 
 
 
@@ -75,74 +88,134 @@ updateGreeting();
 /////////// TODO LIST ////////////
 //////////////////////////////////
 
-
 const todoList = document.querySelector('.todo-list');
 // todos.push(todo);
 
 const initializeTodoList = () => {
-  console.log(todos)
   todoList.innerHTML = '';
 
   todos.forEach(todo => {
     const todoEl = document.createElement('li');
     todoEl.classList.add('todo-item');
+    todoEl.dataset.id = todo.id;
     todoEl.innerHTML = `
-      <div>
+      <div class="todo-item-text">
         <input type="checkbox" ${todo.completed ? 'checked' : ''}>
-        ${todo.text} 
+        <span>${todo.text} </span>
       </div>
-      <div>
-        <button class="delete-todo">Delete</button>
+      <div class="todo-item-tags">
+        <div class="priority ${todo.priority}">${todo.priority} </div>
+      </div>
+      <div class="todo-item-actions">
+        <span class="edit-todo">
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83l3.75 3.75l1.83-1.83z"/></svg>
+        </span>
+        <div class="delete-todo">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.413-.588T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.588 1.413T17 21H7Zm2-4h2V8H9v9Zm4 0h2V8h-2v9Z"/></svg> 
+        </div>
       </div>
       `;
     todoList.appendChild(todoEl);
   });
 }
 
-
+//////////////////////////////////
+///////// ADD TODO FORM //////////
+//////////////////////////////////
 
 const addTodoForm = document.querySelector('.add-todo-form');
 
 addTodoForm.addEventListener('submit', e => {
   e.preventDefault();
 
-  const todoText = addTodoForm.todo.value.trim();
-
-  if (todoText) {
-    todos.push({
-      text: todoText,
-      completed: false,
-    });
-    addTodoForm.reset();
-    chrome.storage.sync.set({ todos });
-    initializeTodoList();
+  if (!addTodoForm.todo.value.trim()) {
+    return;
   }
- 
+
+  const todo = {
+    // generate unique integer id
+    id: Date.now() ,
+    text: addTodoForm.todo.value.trim(),
+    priority: addTodoForm.priority.value,
+    completed: false,
+  }
+
+  todos.push(todo);
+  addTodoForm.reset();
+  chrome.storage.sync.set({ todos });
+  initializeTodoList();
 });
 
-// delete todo
+//////////////////////////////////
+//////// DELETE TODO ITEM ////////
+//////////////////////////////////
 
 todoList.addEventListener('click', e => {
-  if (e.target.classList.contains('delete-todo')) {
+  if (e.target.classList.contains('delete-todo') || e.target.closest('.delete-todo')) {
     const todoItem = e.target.closest('.todo-item');
-    const todoText = todoItem.querySelector('div:first-child').textContent.trim();
-
-    todos.splice(todos.findIndex(todo => todo.text === todoText), 1);
+    const id = todoItem.dataset.id;
+ 
+    const index = todos.findIndex(todo => todo.id == id);
+    todos.splice(index, 1);
     chrome.storage.sync.set({ todos });
     initializeTodoList();
   }
 });
 
 
-// toggle todo completed
+//////////////////////////////////
+///// MARK TODO AS COMPLETED /////
+//////////////////////////////////
 
 todoList.addEventListener('change', e => {
   if (e.target.type === 'checkbox') {
     const todoItem = e.target.closest('.todo-item');
-    const todoText = todoItem.querySelector('div:first-child').textContent.trim();
-
-    todos.find(todo => todo.text === todoText).completed = !todos.find(todo => todo.text === todoText).completed;
+    const id = todoItem.dataset.id;
+    const index = todos.findIndex(todo => todo.id == id);
+    todos[index].completed = !todos[index].completed;
     chrome.storage.sync.set({ todos });
-    initializeTodoList();
   }
 });
+
+
+
+// show dialog to edit todo
+
+const showDialogButton = document.querySelectorAll('.edit-todo');
+
+todoList.addEventListener('click', e => {
+  if (e.target.classList.contains('edit-todo') || e.target.closest('.edit-todo')) {
+    const todoItem = e.target.closest('.todo-item');
+    const id = todoItem.querySelector('div:first-child').dataset.id;
+    const index = todos.findIndex(todo => todo.id === id);
+    const todo = todos[index];
+
+    createDialog();
+  }
+});
+
+
+
+// create Dialog box;
+
+const createDialog = () => {
+  const dialog = document.createElement('div');
+  dialog.classList.add('dialog');
+  dialog.innerHTML = `
+    <div class="dialog-content">
+      <div class="dialog-header">
+        <div class="dialog-title">Edit Todo</div>
+        <div class="dialog-close">X</div>
+      </div>
+      <div class="dialog-body">
+        <form class="edit-todo-form">
+          <input type="text" name="todo" value="Edit todo">
+          <button type="submit">Save</button>
+        </form>
+      </div>
+    </div>
+  `;
+
+  document.body.appendChild(dialog);
+}
+
